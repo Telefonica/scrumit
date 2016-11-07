b8487be13e8fdce02c396d5907830a3d13f97163
Gustavo Garcia <gustavogb@gmail.com>
2016-11-07 14:01:13 +0100

    MISC

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var request = require('request-promise');
 var bot = require('./bot');
 
 var index = require('./routes/index');
@@ -13,35 +14,76 @@ var bigbrother = require('./bigbrother');
 
 var app = express();
 
-
+var CHANNEL = 'general';
 
 bot.on('message', function(data) {
   // mapear messages a controllers
   if (data.type === 'message') {
-    console.log(data.channel, data.text)
+    console.log(data)
+    if (data.subtype != 'bot_message') {
+      getUserInfo(data.user).then((username) => {
+        bot.postMessageToChannel(CHANNEL, data.text, { as_user: false, username: username });
+      });
+    }
   }
 });
 
+var members;
+
+function getMembers(channel) {
+  if (members) {
+    return Promise.resolve(members);
+  }
+
+  console.log('Retrieving channel members');
+  return bot.getChannel(channel).then((data) => {
+    var promises = [];
+    data.members.forEach((userId, i) => {
+      promises[i] = getUserInfo(userId).then((username) => {
+        return username;
+      });
+    });
+    return Promise.all(promises).then((usernames) => {
+      members = usernames;
+      return usernames;
+    });
+  });
+}
+
+function postToEverybody(channel, message) {
+  getMembers(channel).then((usernames) => {
+    console.log(usernames)
+    usernames.forEach((username) => {
+      console.log('Sending', username, message)
+      bot.postMessageToUser(username, message);
+    });
+  });
+}
+
 function startBreak() {
-  bot.postMessageToUser('jorgev', 'Chiste o juego o ...');
+  postToEverybody(CHANNEL, 'Juego, chiste, pregunta....');
   setTimeout(function() {
     startWorking();
   }, 1 * 60 * 1000);
 }
 
-function startWorking() {
-  bot.getChannel('general').then((data) => {
-    console.log(data);
-  });
+var BOT_TOKEN = process.env.BOT_TOKEN;
 
+function getUserInfo(userId) {
+  var url = 'https://slack.com/api/users.info?token=' + BOT_TOKEN + '&user=' + userId;
+  return request.post(url).then((response) => {
+    return JSON.parse(response).user.name;
+  });
+}
+
+function startWorking() {
   bigbrother.askTo('jorgev', function (error, question) {
-    bot.postMessageToUser('jorgev', question);
+    postToEverybody(CHANNEL, question);
     setTimeout(function() {
-      bot.postMessageToUser('jorgev', 'meow!');
+      postToEverybody(CHANNEL, 'Como ha ido?');
       startBreak();
     }, 2 * 60 * 1000);
   });
-  
 }
 
 startWorking();
